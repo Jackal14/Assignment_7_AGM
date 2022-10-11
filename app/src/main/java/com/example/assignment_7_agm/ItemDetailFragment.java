de3c5b6db7d2b7b39aa95f8d683512ccdc188c93
Anthony Moncivais <55060252+Jackal14@users.noreply.github.com>
2022-10-11 08:53:16 -0500

    Initial setup for fab

--- app/src/main/java/com/example/assignment_7_agm/ItemDetailFragment.java
+++ app/src/main/java/com/example/assignment_7_agm/ItemDetailFragment.java
@@ -11,9 +11,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.example.assignment_7_agm.placeholder.PlaceholderContent;
 import com.example.assignment_7_agm.databinding.FragmentItemDetailBinding;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 /**
  * A fragment representing a single Item detail screen.
@@ -35,6 +42,7 @@ public class ItemDetailFragment extends Fragment {
     private PlaceholderContent.PlaceholderItem mItem;
     private CollapsingToolbarLayout mToolbarLayout;
     private TextView mTextView;
+    private FloatingActionButton testingFab;
 
     private final View.OnDragListener dragListener = (v, event) -> {
         if (event.getAction() == DragEvent.ACTION_DROP) {
@@ -71,13 +79,14 @@ public class ItemDetailFragment extends Fragment {
 
         binding = FragmentItemDetailBinding.inflate(inflater, container, false);
         View rootView = binding.getRoot();
-
+        testingFab = rootView.findViewById(R.id.fab);
         mToolbarLayout = rootView.findViewById(R.id.toolbar_layout);
         mTextView = binding.itemDetail;
 
         // Show the placeholder content as text in a TextView & in the toolbar if available.
         updateContent();
         rootView.setOnDragListener(dragListener);
+
         return rootView;
     }
 
@@ -94,5 +103,44 @@ public class ItemDetailFragment extends Fragment {
                 mToolbarLayout.setTitle(mItem.content);
             }
         }
+        if (testingFab != null)
+        {
+            testingFab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view)
+                {
+                    testAllThatJazz();
+                }
+            });
+        }
+
     }
+
+    private void testAllThatJazz() {
+        String url = "https://api.jsonbin.io/v3/b/5f726a107243cd7e8245d58b";  // THAT should be in a strings.xml file!
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(getActivity());
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the response string in our convenient existing text view
+                        mTextView.setText("Response is: "+ response);
+                        // NEXT, we need to use GSON to turn that JSON into a model
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // you should drop a breakpoint RIGHT HERE if you need to see the error coming back
+                mTextView.setText("That didn't work!");
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+
 }
